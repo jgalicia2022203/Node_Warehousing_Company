4e66c7fceeb4e5b9d6769e9549255b465e08c998
jgalicia <jgalicia-2022203@kinal.edu.gt>
2024-04-24 20:54:53 -0600

    Model Schema finished

---
+++ src/tasks/task.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    default: Date.now,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["complete", "incomplete"],
+    default: "incomplete",
+  },
+  creator_name: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Task", taskSchema);
