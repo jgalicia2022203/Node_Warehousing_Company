40a1da982066a12122bd7d84fface85ba3e5b562
apalma <apalma-2022125@kinal.edu.gt>
2024-04-30 15:41:19 -0600

    Se finalizo update y delete

--- src/tasks/task.controller.js
+++ src/tasks/task.controller.js
@@ -1,33 +1,81 @@
-'strick'
+"strick";
 
 import { response, request } from "express";
-import Tasks from './task.model.js';
+import Tasks from "./task.model.js";
 
-export const taskGet = async (req = request, res = response) =>{
-    const {limite, desde} = req.params;
-    const query = {status: 'incomplete'}
- 
-    const [total, tasks] = await Promise.all([
-        Tasks.countDocuments(query),
-        Tasks.find(query)
-        .skip(Number(desde))
-        .limit(Number(limite))
-    ])
- 
-    res.status(200).json({
-        total,
-        tasks
-    })
-}
+export const taskGet = async (req = request, res = response) => {
+  const { limite, desde } = req.params;
+  const query = { status: "incomplete" };
+
+  const [total, tasks] = await Promise.all([
+    Tasks.countDocuments(query),
+    Tasks.find(query).skip(Number(desde)).limit(Number(limite)),
+  ]);
+
+  res.status(200).json({
+    total,
+    tasks,
+  });
+};
 
 export const taskPost = async (req, res) => {
-    const { name, description, startDate, endDate,creator_name } = req.body;
-    const tasks = new Tasks ({ name, description, startDate, endDate,creator_name });
+  const { name, description, startDate, endDate, creator_name } = req.body;
+  const tasks = new Tasks({
+    name,
+    description,
+    startDate,
+    endDate,
+    creator_name,
+  });
 
-    await tasks.save();
+  await tasks.save();
 
-    res.status(200).json({
-        tasks
+  res.status(200).json({
+    tasks,
+  });
+};
+
+
+export const taskUpdate = async (req, res) => {
+  try {
+    const { name, description, startDate, endDate, creator_name } = req.body;
+    const { id } = req.params;
+    const task = await Tasks.findById(id); // Cambiado de 'tasks' a 'task'
+
+    if (!task) {
+      return res.status(404).json({ msg: 'Task not found' });
+    }
+
+    if (name) task.name = name;
+    if (description) task.description = description;
+    if (startDate) task.startDate = startDate;
+    if (endDate) task.endDate = endDate;
+    if (creator_name) task.creator_name = creator_name;
+
+    await task.save();
+
+    return res.status(200).json({
+      msg: 'The task has been updated',
+      task,
     });
-}
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('The task has not been updated');
+  }
+};
 
+
+export const taskDelete = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const task = await Tasks.findByIdAndUpdate(id, {status: "complete"});
+    res.status(200).json({
+      msg: "The task has been delete",
+      task
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("The task has not ben elemete");
+  }
+};
--- src/tasks/task.routes.js
+++ src/tasks/task.routes.js
@@ -1,30 +1,45 @@
 import { Router } from "express";
 import { check } from "express-validator";
 
-import {
-    taskPost
-} from "./task.controller.js";
+import { taskDelete, taskGet, taskPost, taskUpdate } from "./task.controller.js";
 
-import {tasksExist} from "../common/helpers/db-validators.js";
-import {  } from "../common/middlewares/validate-fields.js";
+import { tasksExist } from "../common/helpers/db-validators.js";
+import {} from "../common/middlewares/validate-fields.js";
 
 const router = Router();
 
-
-router.get("/", taskGet)
+router.get("/", taskGet);
 
 router.post(
-    "/",
-    [
+  "/",
+  [
     check("name").custom(tasksExist),
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("description", "Description is required").not().isEmpty(),
     check("startDate", "startDate is required").not().isEmpty(),
-    check("endDate","endDate is required").not().isEmpty(),
+    check("endDate", "endDate is required").not().isEmpty(),
     check("creator_name", "The creator is required").not().isEmpty(),
-    
-    ],
-    taskPost
+  ],
+  taskPost
+);
+
+router.put(
+  "/:id", 
+  [
+    check("id", "The id is not valid").isMongoId(),
+    check("Creator_name").optional(), 
+  ],
+  taskUpdate
+);
+
+
+router.delete(
+  "/:id",
+  [
+    check("id", "The id is required").not().isEmpty(),
+    check("creador", "The Creator_Name is required").not().isEmpty(),
+  ],
+  taskDelete
 );
 
 export default router;
